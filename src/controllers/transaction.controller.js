f314ffde513c9dfca5ca07c45b988b6e979eac70
Emanuel Ramos <emanuelramos19@outlook.com>
2025-04-21 20:23:34 -0300

    Transactions soporte

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,178 +1,319 @@
-import mongoose from "mongoose"
-import ApiResponse from "../helpers/api.response.helper.js"
-import AppError from "../helpers/error.helpers.js"
-import Validations from "../helpers/validation.helpers.js"
-import Transaction from "../models/transaction.model.js"
-import TransactionRepository from "../repositories/transaction.repository.js"
+import mongoose from "mongoose";
+import ApiResponse from "../helpers/api.response.helper.js";
+import AppError from "../helpers/error.helpers.js";
+import Validations from "../helpers/validation.helpers.js";
+import Transaction from "../models/transaction.model.js";
+import TransactionRepository from "../repositories/transaction.repository.js";
 
 export const addTransactionController = async (req, res, next) => {
     try {
-        const userId = req.user.id
+        const userId = req.user.id;
         if (!userId) {
-            return next(new AppError('Falta el id de usuario', 400))
+            return next(new AppError("Falta el id de usuario", 400));
         }
 
-        const { type, category, amount, description } = req.body
+        const { type, category, amount, description } = req.body;
         if (!type || !category || !amount) {
-            return next(new AppError('Hay campos que son obligatorios', 400))
+            return next(new AppError("Hay campos que son obligatorios", 400));
         }
 
-        const validTypes = ["income", "expense"]
+        const validTypes = ["income", "expense"];
         if (!validTypes.includes(type)) {
-            return next(new AppError('El tipo de transaction no es valido', 400))
+            return next(
+                new AppError("El tipo de transaction no es valido", 400)
+            );
         }
 
-        const validador = new Validations({ category, amount, description })
+        const validador = new Validations({ category, amount, description });
 
         validador
-            .isString('category').min_max_length('category', 0, 20)
-            .isString('description').min_max_length('description', 0, 100)
-            .isNumber('amount')
+            .isString("category")
+            .min_max_length("category", 0, 20)
+            .isString("description")
+            .min_max_length("description", 0, 100)
+            .isNumber("amount");
 
-        const errores = validador.obtenerErrores()
+        const errores = validador.obtenerErrores();
         if (errores.length > 0) {
-            return next(new AppError('Errores de validacion', 400, errores))
+            return next(new AppError("Errores de validacion", 400, errores));
         }
 
-        const amuntEnCentavos = Math.round(Number(amount) * 100)
+        const amuntEnCentavos = Math.round(Number(amount) * 100);
 
         const newTransaction = {
             userId: userId,
             type: type,
             category: category,
             amount: amuntEnCentavos,
-            description: description
-        }
+            description: description,
+        };
 
-        await TransactionRepository.createTransaction(newTransaction)
+        await TransactionRepository.createTransaction(newTransaction);
 
-        return res.status(201).json(new ApiResponse(201, 'Transaction creada con exito', newTransaction))
-
+        return res
+            .status(201)
+            .json(
+                new ApiResponse(
+                    201,
+                    "Transaction creada con exito",
+                    newTransaction
+                )
+            );
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 export const getTransactionController = async (req, res, next) => {
     try {
-        const user = req.user.id
+        const user = req.user.id;
         if (!user) {
-            return next(new AppError('Falta el id de usuario', 400))
+            return next(new AppError("Falta el id de usuario", 400));
         }
 
-        const userId = mongoose.Types.ObjectId.createFromHexString(user)
-        const filter = { userId }
+        const userId = mongoose.Types.ObjectId.createFromHexString(user);
+        const filter = { userId };
 
-        const { type, category, starDate, endDate } = req.body
+        const { type, category, startDate, endDate } = req.body;
 
         if (type) {
             if (!["income", "expense"].includes(type)) {
-                return next(new AppError('El tipo de transaction no es valido', 400))
+                return next(
+                    new AppError("El tipo de transaction no es valido", 400)
+                );
             }
-            filter.type = type
+            filter.type = type;
         }
 
         if (category) {
-            filter.category = category
+            filter.category = category;
         }
 
-        if (starDate || endDate) {
-            if (typeof starDate !== 'string' || typeof endDate !== 'string') {
-                return next(new AppError('Las fechas deben ser en formato "YYYY-MM-DD"', 400))
+        if (startDate || endDate) {
+            if (typeof startDate !== "string" || typeof endDate !== "string") {
+                return next(
+                    new AppError(
+                        'Las fechas deben ser en formato "YYYY-MM-DD"',
+                        400
+                    )
+                );
             }
 
-            const start = new Date(starDate)
-            const end = new Date(endDate)
+            const start = new Date(startDate);
+            const end = new Date(endDate);
             if (isNaN(start.getTime()) && isNaN(end.getTime())) {
-                return next(new AppError('Formato fecha invalido', 400))
-
+                return next(new AppError("Formato fecha invalido", 400));
             }
-            start.setUTCHours(0, 0, 0, 0)
-            end.setUTCHours(23, 59, 59, 999)
+            start.setUTCHours(0, 0, 0, 0);
+            end.setUTCHours(23, 59, 59, 999);
             filter.date = {
-                $gte: start, $lte: end
-            }
+                $gte: start,
+                $lte: end,
+            };
         }
 
-        const transactions = await TransactionRepository.findTransaction(filter)
+        const transactions = await TransactionRepository.findTransaction(
+            filter
+        );
 
         if (!transactions.length) {
-            return next(new AppError('No se encontro una transaction con esos parametros', 400))
+            return next(
+                new AppError(
+                    "No se encontro una transaction con esos parametros",
+                    400
+                )
+            );
         }
-        
-        return res.status(200).json(new ApiResponse(200, 'Busqueda realizada con exito', transactions))
 
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    "Busqueda realizada con exito",
+                    transactions
+                )
+            );
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 export const updateTransactionController = async (req, res, next) => {
     try {
-        const userId = req.user.id
+        const userId = req.user.id;
         if (!userId) {
-            return next(new AppError('Falta el id de usuario', 400))
+            return next(new AppError("Falta el id de usuario", 400));
         }
 
-        const { id } = req.params
+        const { id } = req.params;
         if (!id) {
-            return next(new AppError('Falta id de la transaction', 400))
+            return next(new AppError("Falta id de la transaction", 400));
         }
 
-        const { type, category, amount, date, description } = req.body
+        const { type, category, amount, date, description } = req.body;
 
-        const validatior = new Validations({ category, amount, description })
+        const validatior = new Validations({ category, amount, description });
 
-        const update = {}
+        const update = {};
 
         if (type) {
             if (!["income", "expense"].includes(type)) {
-                return next(new AppError('El tipo de transaction no es valido', 400))
+                return next(
+                    new AppError("El tipo de transaction no es valido", 400)
+                );
             }
-            update.type = type
+            update.type = type;
         }
 
         if (category) {
-            update.category = category
-            validatior.isString('category').min_max_length('category', 0, 20)
+            update.category = category;
+            validatior.isString("category").min_max_length("category", 0, 20);
         }
 
         if (amount) {
-            update.amount = amount
-            validatior.isNumber('amount')
+            update.amount = amount;
+            validatior.isNumber("amount");
         }
 
         if (description) {
-            update.description = description
-            validatior.isString('description').min_max_length('description', 0, 100)
+            update.description = description;
+            validatior
+                .isString("description")
+                .min_max_length("description", 0, 100);
         }
 
-        const errores = validatior.obtenerErrores()
+        const errores = validatior.obtenerErrores();
         if (errores.length > 0) {
-            return next(new AppError('Error de validacion', 400, errores))
+            return next(new AppError("Error de validacion", 400, errores));
         }
 
         if (date) {
-            const newDate = new Date(date)
+            const newDate = new Date(date);
             if (!isNaN(newDate.getTime())) {
-                newDate.getUTCHours(0, 0, 0, 0)
-                update.date = newDate
+                newDate.getUTCHours(0, 0, 0, 0);
+                update.date = newDate;
             } else {
-                return next(new AppError('Formato fecha invalido', 400))
+                return next(new AppError("Formato fecha invalido", 400));
             }
         }
 
-        const amuntEnCentavos = Math.round(Number(update.amount) * 100)
-        update.amount = amuntEnCentavos
+        const amuntEnCentavos = Math.round(Number(update.amount) * 100);
+        update.amount = amuntEnCentavos;
 
-        const transaction = TransactionRepository.updateTransaction(id, userId, update)
+        const transaction = TransactionRepository.updateTransaction(
+            id,
+            userId,
+            update
+        );
 
         if (!transaction) {
-            return next(new AppError('No se encontro la transaction', 400))
+            return next(new AppError("No se encontro la transaction", 400));
         }
 
-        return res.status(200).json(new ApiResponse(200, 'Update con exito', transaction))
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "Update con exito", transaction));
+    } catch (error) {
+        next(error);
+    }
+};
 
+export const getTransactionMovementsController = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        if (!userId) {
+            return next(new AppError("Falta id del usuario", 400))
+        }
+
+        const { startDate, endDate } = req.query
+
+        let dateFilter = {}
+
+        if (startDate && endDate) {
+            const start = new Date(startDate)
+            const end = new Date(endDate)
+            start.setUTCHours(0, 0, 0, 0)
+            end.setUTCHours(23, 59, 59, 999)
+
+            dateFilter.date = { $gte: start, $lte: end }
+        }
+
+        const filter = {
+            userId: mongoose.Types.ObjectId.createFromHexString(userId),
+            ...dateFilter
+        }
+
+        const transaction = await Transaction.find(filter)
+
+        const totals = transaction.reduce(
+            (acc, tx) => {
+                const amount = tx.amount / 100;
+                if (tx.type === "income") acc.income += amount;
+                else if (tx.type === "expense") acc.expense += amount;
+                return acc;
+            },
+            { income: 0, expense: 0 }
+        );
+        const balance = totals.income - totals.expense
+
+        return res.status(200).json(new ApiResponse(200, "Total calculado con exito", {
+            ...totals, balance
+        }))
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getTransactionCategory = async (req, res, next) => {
+    try {
+        const userId = req.user.id;
+        if (!userId) {
+            return next(new AppError("Falta el id de usuario", 400));
+        }
+
+        const {startDate, endDate} = req.query
+
+        let matchFilter = {
+            userId: mongoose.Types.ObjectId.createFromHexString(userId)
+        };
+
+        if (startDate && endDate) {
+            const start = new Date(startDate)
+            const end = new Date(endDate)
+            start.setUTCHours(0, 0, 0, 0)
+            end.setUTCHours(23, 59, 59, 999)
+
+            matchFilter.date = { $gte: start, $lte: end }
+        }
+
+        const sumary = await Transaction.aggregate([
+            {$match: matchFilter},
+            {
+                $group:{
+                    _id: {category: "$category", type: "$type"},
+                    total: {$sum: "$amount"}
+                }
+            }
+        ])
+
+        const result = {}
+        sumary.forEach((item)=>{
+            const category = item._id.category
+            const type = item._id.type
+            const amount = item.total /100
+
+            const sing = type === "income"?"+":"-"
+            const formattedAmount = `${sing}${amount}`
+
+            if (!result[category]) {
+                result[category]=[formattedAmount]
+            }else{
+                result[category].push(formattedAmount)
+            }
+        })
+
+        return res.status(200).json(new ApiResponse(200, "Resumen por categoria con exito", result))
     } catch (error) {
         next(error)
     }
@@ -180,26 +321,32 @@ export const updateTransactionController = async (req, res, next) => {
 
 export const deleteTransactionController = async (req, res, next) => {
     try {
-        const userId = req.user.id
+        const userId = req.user.id;
         if (!userId) {
-            return next(new AppError('Falta el id de usuario', 400))
+            return next(new AppError("Falta el id de usuario", 400));
         }
 
-        const { id } = req.params
+        const { id } = req.params;
 
         if (!id) {
-            return next(new AppError('Falta id de la transaction', 400))
+            return next(new AppError("Falta id de la transaction", 400));
         }
 
-        const transaction = await TransactionRepository.deleteTransaction(id, userId)
+        const transaction = await TransactionRepository.deleteTransaction(
+            id,
+            userId
+        );
 
         if (!transaction) {
-            return next(new AppError('No se encontro la transaction a eliminar', 400))
+            return next(
+                new AppError("No se encontro la transaction a eliminar", 400)
+            );
         }
 
-        return res.status(200).json(new ApiResponse(200, 'Eliminado con exito'))
-
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "Eliminado con exito"));
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -3,7 +3,9 @@ import authMiddleware from '../middlewares/auth.middleware.js';
 import {
     addTransactionController,
     deleteTransactionController,
+    getTransactionCategory,
     getTransactionController,
+    getTransactionMovementsController,
     updateTransactionController,
 }
 from '../controllers/transaction.controller.js';
@@ -12,6 +14,8 @@ const transactionRoute = express.Router()
 
 transactionRoute.post('/', authMiddleware, addTransactionController)
 transactionRoute.get('/search', authMiddleware, getTransactionController)
+transactionRoute.get('/movements', authMiddleware, getTransactionMovementsController)
+transactionRoute.get('/category', authMiddleware, getTransactionCategory)
 transactionRoute.put('/update/:id', authMiddleware, updateTransactionController)
 transactionRoute.delete('/delete/:id', authMiddleware, deleteTransactionController)
 
